8b81ee11987891d102447f1932a81bee67fe1154
ranaabz <153768755+ranaabz@users.noreply.github.com>
2025-07-18 22:39:24 +0300

    Update AdminPanel.php

--- AdminPanel.php
+++ AdminPanel.php
@@ -1,17 +1,15 @@
 <?php
-$host = "localhost";
-$user = "root"; // Change if needed
-$password = "";
-$database = "chroma_spark"; 
+$host = getenv("DB_HOST") ?: "localhost";
+$port = getenv("DB_PORT") ?: 3306;
+$user = getenv("DB_USER") ?: "root";
+$password = getenv("DB_PASS") ?: "";
+$database = getenv("DB_NAME") ?: "chroma_spark";
 
 // Create connection
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    // Connection successful (optional: for debugging)
-    // echo("Connection success");
 }
 ?>
