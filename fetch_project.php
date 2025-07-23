c3975105d51c05c119d7205713ac93d5f1fdd7d8
ranaabz <153768755+ranaabz@users.noreply.github.com>
2025-07-23 12:18:46 +0300

    Update fetch_project.php

--- fetch_project.php
+++ fetch_project.php
@@ -1,13 +1,22 @@
 <?php
-include 'AdminPanel.php';
+include 'connection.php'; // Should set $dbconn as PostgreSQL connection resource
+
+header('Content-Type: application/json');
 
 $sql = "SELECT * FROM projects ORDER BY created_at DESC";
-$result = $conn->query($sql);
+$result = pg_query($dbconn, $sql);
+
+if (!$result) {
+    echo json_encode(["error" => "SQL Error: " . pg_last_error($dbconn)]);
+    exit;
+}
 
 $projects = [];
-while ($row = $result->fetch_assoc()) {
+while ($row = pg_fetch_assoc($result)) {
     $projects[] = $row;
 }
 
 echo json_encode($projects);
+
+pg_close($dbconn);
 ?>
