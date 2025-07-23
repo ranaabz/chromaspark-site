2faf2ec86d2990bd2e4243cf6990a0f40fa60064
ranaabz <153768755+ranaabz@users.noreply.github.com>
2025-07-23 15:11:51 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,14 @@
 <?php
-// PostgreSQL connection using pg_connect
-$host = "dpg-d209sfre5dus73d5rfsg-a";    // e.g., "localhost" or your Render host
-$port = "5432";            // default PostgreSQL port
-$dbname = "chroma_spark_nfki";
-$user = "chroma_spark_nfki_user";
-$password = "MJfUXNzs4727vpq98rdbLtiPRDVTrILE";
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT") ?: "5432";
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
 $dbconn = pg_connect($conn_string);
 
 if (!$dbconn) {
-    die("Error in connection: " . pg_last_error());
+    die("❌ Error in connection: " . pg_last_error());
 }
 ?>
